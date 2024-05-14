4ff7087f538b9fd6b2b00928d4a82b55d233da59
tlevi101 <70849019+tlevi101@users.noreply.github.com>
2024-05-14 21:52:58 +0200

    Add project files.

---
+++ PCMonitoringNetConsoleApp/Utils/MemoryMonitoring.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCMonitoringConsoleApp.Utils
+{
+    internal class MemoryMonitoring : Monitoring
+    {
+        private double memoryUsed;
+        private double maxMemory;
+
+        public MemoryMonitoring() : base()
+        {
+            maxMemory = new Microsoft.VisualBasic.D;
+        }
+        public override void updateState()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
